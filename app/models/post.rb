ead1e6e51bf43f1c93231c601d1d4ef2c9f75b13
Hyder <hyderali400@gmail.com>
2023-06-15 16:37:53 +0530

    fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,13 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-  
+
   after_save :update_user_posts_counter
 
-   # Updates the posts counter for a user
+  # Updates the posts counter for a user
 
-   private 
-   
+  private
+
   def update_user_posts_counter
     author.update(posts_counter: author.posts.count)
   end
