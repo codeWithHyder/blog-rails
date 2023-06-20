952a806ff3a5b62c70a3947863e088b3ca25e62f
Hyder <hyderali400@gmail.com>
2023-06-20 23:27:51 +0530

    add controller spec to rails blog app

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe Like, type: :model do
       Post.create(
         title: 'Test Post',
         text: 'This is a test post',
-        user:,
+        user: user,
         comment_counter: 0,
         likes_counter: 0
       )
@@ -33,8 +33,8 @@ RSpec.describe Like, type: :model do
 
     subject do
       Like.new(
-        user:,
-        post:
+        user: user,
+        post:post
       )
     end
 
