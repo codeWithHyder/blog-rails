3adf70b2c29fd7ca81c522d5629da416dc4dc975
Hyder <hyderali400@gmail.com>
2023-06-28 15:15:24 +0530

    fix rubocop linters

--- app/models/login.rb
+++ app/models/login.rb
@@ -1,5 +1,5 @@
 class Login < ApplicationRecord
-   #:confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 end
