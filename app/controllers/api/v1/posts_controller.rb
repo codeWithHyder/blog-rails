e94dbe858bf0b52e33c279b86ef68a4baf6d1cba
Hyder <hyderali400@gmail.com>
2023-06-30 20:36:29 +0530

    api documention

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,12 +1,11 @@
 module Api
   module V1
     class PostsController < ApplicationController
-      def index
-        user = User.find(params[:user_id])
-        posts = user.posts
-
-        render json: posts
-      end
+       def index
+      @user = User.find(params[:user_id])
+      @posts = Post.where(user_id: @user.id).includes(:comments)
+      render json: @posts
     end
+  end
   end
 end
